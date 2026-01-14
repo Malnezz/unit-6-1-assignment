1e6c67b02e2309274dad905e7693980d0a9b4c8f
Malice Nguyen <malnezznguyen@gmail.com>
2026-01-14 19:05:08 +0000

    asd

--- Main.java
+++ Main.java
@@ -5,17 +5,28 @@ public class Main
   public static void main(String[] args)
   {
     // Problem 1 - Create a scanner and an array of size 3
+    Scanner vals = new Scanner(System.in);
+    double[] valsArray = new double[3]; 
+
+    for (int i = 0; i < valsArray.length; i++) 
+    {
+      valsArray[i] = i;
+      System.out.print(i + " ");
+    }
+      
+    // Print the array to verify
+
 
     // Problem 2
     // Use the scanner to get user input and give it to the method
 
-    int N = /* scan.nextInt() */;
-    int[] output = sumsOfNums(N);
+    /* int N = /* scan.nextInt() */
+    /* int[] output = sumsOfNums(N); */ 
   }
 
-  public static int[] sumsOfNums(int input)
+ /* public static int[] sumsOfNums(int input)
   {
     // Implement Problem 2 here
     return 0;
-  }
+  } */ 
 }
